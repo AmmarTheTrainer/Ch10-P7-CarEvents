207f7bbcb13e4febf519fe192c661e974a2431d1
Ammar The Trainer <ammarthetrainer@gmail.com>
2018-10-01 19:09:42 +0500

    final commit from office

--- Ch10-P7-CarEvents/Program.cs
+++ Ch10-P7-CarEvents/Program.cs
@@ -10,7 +10,55 @@ namespace Ch10_P7_CarEvents
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("***** Fun with Events *****\n");
 
+            Car c1 = new Car("SlugBug", 100, 10);
+            //// Register event handlers.
+            //c1.AboutToBlow += new Car.CarEngineHandler(CarIsAlmostDoomed);
+            //c1.AboutToBlow += new Car.CarEngineHandler(CarAboutToBlow);
+
+            //Car.CarEngineHandler d = new Car.CarEngineHandler(CarExploded); // assigning delegate a method
+
+            //c1.Exploded += d; // assigning event the method
+
+            //Console.WriteLine("***** Speeding up *****");
+            //for (int i = 0; i < 6; i++)
+            //    c1.Accelerate(20);
+
+            //// Remove CarExploded method
+            //// from invocation list.
+            //c1.Exploded -= d;
+            //Console.WriteLine("\n***** Speeding up *****");
+
+            //for (int i = 0; i < 6; i++)
+            //    c1.Accelerate(20);
+
+            // Register event handlers.
+            c1.AboutToBlow += CarIsAlmostDoomed;
+            c1.AboutToBlow += CarAboutToBlow;
+
+            c1.Exploded += CarExploded;  
+
+            Console.WriteLine("***** Speeding up *****");
+            for (int i = 0; i < 6; i++)
+                c1.Accelerate(20);
+
+            c1.Exploded -= CarExploded;
+
+            Console.WriteLine("\n***** Speeding up *****");
+            for (int i = 0; i < 6; i++)
+                c1.Accelerate(20);
+
+            Console.ReadLine();
         }
+
+        
+
+        public static void CarAboutToBlow(string msg)
+        { Console.WriteLine(msg); }
+        public static void CarIsAlmostDoomed(string msg)
+        { Console.WriteLine("=> Critical Message from Car: {0}", msg); }
+        public static void CarExploded(string msg)
+        { Console.WriteLine(msg); }
     }
 }
